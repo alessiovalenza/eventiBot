11432ab6328959e5d0588329cd46ae6c2e72c1b4
Alessio Valenza <alesvale97@gmail.com>
2018-08-30 10:26:17 +0200

    eventi_bot and my_parser updated

--- eventi_bot.py
+++ eventi_bot.py
@@ -2,57 +2,42 @@ from telegram import InlineQueryResultArticle, InputTextMessageContent
 from telegram.ext import Updater, CommandHandler, InlineQueryHandler
 from my_parser import Parser
 from uuid import uuid4
-import logging
 
 TOKEN = "637115124:AAHbAtbnTwEO10RutD0PmKSR8_tbz17i2R4"
-
+# TOKEN = "669249562:AAGtdMSBoQe4PkG7jujjdeC2ktRlBMOkgN0"
 
 def handle_start(bot, update):
-    update.message.reply_text("Ciao, dimmi il nome di un luogo e troverò gli eventi nelle vicinanze per te")
-
-
-def error(bot, update, e):
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                        level=logging.INFO)
-    logger = logging.getLogger(__name__)
-    logger.warning('Update {} caused error {}'.format(update, e))
-
+    update.message.reply_text("Dimmi il nome di un luogo e troverò gli eventi nelle vicinanze per te")
 
 def inline_place(bot, update):
     query = update.inline_query.query
-    help_line = 'Scrivi il nome di una città per cercare solo gli eventi in quella città.' \
-                'Puoi anche cercare in una intera regione!Oppure puoi cercare nei dintorni ' \
-                'di una città scrivendo "dintorni" dopo il nome della città'
-
-    if query == 'help':
-        results = [
-            InlineQueryResultArticle(
-                id=uuid4(),
-                title=help_line,
-                input_message_content=InputTextMessageContent("Un attimo, sto usando @cercaEventi_bot")
-            )
-        ]
-        update.inline_query.answer(results)
+    help = 'Scrivi il nome di una città per cercare solo gli eventi in quella città.\nPuoi anche cercare in una intera regione!\nOppure puoi cercare nei dintorni di una città scrivendo "dintorni" dopo il nome della città'
+    if not query or len(query)==0:
         return
-    if not query or len(query) == 1:
-        results = [
-            InlineQueryResultArticle(
-                id=uuid4(),
-                title='Nessun evento trovato per {}'.format(query),
-                input_message_content=InputTextMessageContent('Nessun evento trovato')
-            )
-        ]
     else:
+        if query == 'help':
+            results = [
+                InlineQueryResultArticle(
+                    id=uuid4(),
+                    title=help,
+                    input_message_content=InputTextMessageContent("Un attimo, sto usando @cercaEventi_bot")
+                )
+            ]
+            update.inline_query.answer(results)
+            return
         p = Parser(query)
         events = p.getEvents()
-        if len(events) > 0:
+        if len(events)>0:
             results = [
                 InlineQueryResultArticle(
                     id=uuid4(),
                     title=event['name'],
-                    input_message_content=InputTextMessageContent("{}\n{}".format(event['name'],event['link'])),
+                    input_message_content=InputTextMessageContent("{}\n<b>{}</b>\n<b>{}</b>\n{}".format(event['name'],event['date'],event['place'],event['link']),
+                            parse_mode='HTML'),
+                    thumb_url = event['img'],
+                    description = "{}\n{}".format(event['date'],event['place'])
                 )
-                for event in events]
+            for event in events]
         else:
             results = [
                 InlineQueryResultArticle(
@@ -61,8 +46,7 @@ def inline_place(bot, update):
                     input_message_content=InputTextMessageContent('Nessun evento trovato per {}'.format(query))
                 )
             ]
-    update.inline_query.answer(results)
-
+        update.inline_query.answer(results)
 
 def main():
     updater = Updater(token=TOKEN)
@@ -71,11 +55,9 @@ def main():
     inline_place_handler = InlineQueryHandler(inline_place)
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(inline_place_handler)
-    dispatcher.add_error_handler(error)
     updater.start_polling()
     print('Listening...')
     updater.idle()
-
 
 if __name__ == '__main__':
     main()
--- my_parser.py
+++ my_parser.py
@@ -1,38 +1,58 @@
 from requests_html import HTMLSession
 
+class Parser():
 
-class Parser:
 
     def __init__(self, place):
         self.events = []
         self.session = HTMLSession()
         processed_place = self.process_query(place)
-        # Decide between "-" and "" to replace spaces where the place's name has multiple words
         if "dintorni" in processed_place:
-            processed_place = processed_place.replace("dintorni", "").strip()
+            processed_place = processed_place.replace("dintorni","").strip()
             if " " in processed_place:
-                self.request = self.session.get("https://iltaccodibacco.it/{}/"
-                                                .format(processed_place.replace(" ", "-")))
+            #Decide between "-" and "" to replace spaces where the place's name has multiple words
+                self.request = self.session.get("https://iltaccodibacco.it/{}/".format(processed_place.replace(" ","-")))
                 for h in self.request.html.find("h2"):
                     if h.text == "OPS!":
-                        self.request = self.session.get("https://iltaccodibacco.it/{}/"
-                                                        .format(processed_place.replace(" ", "")))
+                        self.request = self.session.get("https://iltaccodibacco.it/{}/".format(processed_place.replace(" ","")))
                         break
             else:
                 self.request = self.session.get("https://iltaccodibacco.it/{}/".format(processed_place))
         else:
             self.request = self.session.get("https://iltaccodibacco.it/?md=Ricerca&des_ricerca={}&c_all=1"
-                                            .format(processed_place.strip().replace(" ", "+")))
+                .format(processed_place.strip().replace(" ","+")))
+
 
     def getEvents(self):
-        links = self.request.html.find('a')
-        already_found = []
-        for link in links:
-            if 'name' in link.attrs and link.attrs['name'].isnumeric() and len(link.text)>0:
-                if link.attrs['href'] not in already_found:
-                    already_found.append(link.attrs['href'])
-                    self.events.append({'name':link.text, 'link':link.attrs['href']})
+        events = self.request.html.find('div')
+        for event in events:
+            if 'class' in event.attrs:
+                if list(event.attrs['class'])[0]=='evento-normale':
+                    event_details = {}
+                    divs = event.find('div')
+                    for div in divs:
+                        if 'class' in div.attrs:
+                            class_type = list(div.attrs['class'])[0]
+                            if class_type == 'evento-img':
+                                a = div.find('a')[0]
+                                img = div.find('img')[0]
+                                event_details['name'] = img.attrs['alt']
+                                event_details['link'] = a.attrs['href']
+                                event_details['img'] = img.attrs['src']
+                            elif class_type == 'evento-data':
+                                event_details['place'] = div.text
+                            elif class_type == 'testa':
+                                event_details['date'] = self.parse_date(div.text)
+                    self.events.append(event_details)
         return self.events
+
 
     def process_query(self, place):
         return ' '.join([elem.strip() for elem in place.lower().split(' ') if elem.strip() != ''])
+
+    def parse_date(self, date_str):
+        if "al" in date_str:
+            words = date_str.replace(",","").replace("-","").replace("l'", "l ").split("al")
+            return "{} - {}".format(words[1].strip(), " ".join(words[2].strip().split(" ")[:3]))
+        else:
+            return " ".join(date_str.split(" ")[1:])
